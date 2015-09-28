a680c3a3e12a7cab8de9e7a8f0a4ff4ea185e622
kagansari <kagannsari@gmail.com>
2015-09-28 16:43:03 +0300

    Signed-off-by: kagansari <kagannsari@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,6 @@ module MartmaraBackend
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     config.assets.precompile += %w(.svg .eot .woff .ttf)
 
     config.generators do |g|
